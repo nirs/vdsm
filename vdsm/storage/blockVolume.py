5f844abc6600e73fc019030060589bbb2035bc02
Nir Soffer <nsoffer@redhat.com>
2016-03-12 04:19:10 +0200

    fileUtils: Remove the redundant "d" mode
    When creating a DirectFile, the only available mode is directio, so the
    non-standard "d" mode is not helpful, and error checking can be
    simplified.
    
    Change-Id: I23be87699af290bb20381219fe32d06bb79ba9b5
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- vdsm/storage/blockVolume.py
+++ vdsm/storage/blockVolume.py
@@ -239,7 +239,7 @@ class BlockVolumeManifest(volume.VolumeManifest):
         data += "\0" * (volume.METADATA_SIZE - len(data))
 
         metavol = lvm.lvPath(vgname, sd.METADATA)
-        with fileUtils.DirectFile(metavol, "r+d") as f:
+        with fileUtils.DirectFile(metavol, "r+") as f:
             f.seek(offs * volume.METADATA_SIZE)
             f.write(data)
 
