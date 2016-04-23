184d42d5b40b034a2cd1fa6f7519024c7f2c5d20
Nir Soffer <nsoffer@redhat.com>
2016-04-24 01:05:33 +0300

    mount: Remove pointless force=True usage
    Using force=True when unmounting loop device is pointless - this option
    will cause umount(8) to call the umount(2) syscall with the MNT_FORCE
    flag, which is useful only for NFS mounts.
    
    Change-Id: I31ff97d882f292f9c6325903ed9072b7146f682e
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- tests/mountTests.py
+++ tests/mountTests.py
@@ -133,7 +133,7 @@ class MountTests(TestCaseBase):
                 try:
                     self.assertTrue(m.isMounted())
                 finally:
-                    m.umount(force=True, freeloop=True)
+                    m.umount(freeloop=True)
                     # TODO: Use libudev to wait for specific event
                     with stopwatch("Wait for udev events"):
                         udevadm.settle(5)
@@ -157,7 +157,7 @@ class MountTests(TestCaseBase):
             try:
                 self.assertTrue(m.isMounted())
             finally:
-                m.umount(force=True, freeloop=True)
+                m.umount(freeloop=True)
                 # TODO: Use libudev to wait for specific event
                 with stopwatch("Wait for udev events"):
                     udevadm.settle(5)
