5efa40e4649ffa186f5e51ba370bd0ecf1c626c5
Nir Soffer <nsoffer@redhat.com>
2016-12-09 17:46:54 +0200

    xleases: Separate VolumeIndex loading from file
    Previously VolumeIndex was created with a file, and loaded data from the
    file when creating an index. This is not needed when formatting a new
    index.
    
    Extract VolumeIndex.load() for loading data into the index and use it
    when creating a VolumeIndex from existing volume.
    
    Change-Id: Ibe0989fbe0381a2f59df847b4d95fe0f3881ef2d
    Bug-Url: https://bugzilla.redhat.com/1317429
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- tests/storage_xlease_test.py
+++ tests/storage_xlease_test.py
@@ -375,8 +375,9 @@ def make_leases():
 
 
 def write_records(records, lockspace, file):
-    index = xlease.VolumeIndex(file)
+    index = xlease.VolumeIndex()
     with utils.closing(index):
+        index.load(file)
         for recnum, record in records:
             block = index.copy_record_block(recnum)
             with utils.closing(block):
