8517a87bf1d8c316db8c53e6df9763913812518d
Nir Soffer <nsoffer@redhat.com>
2016-12-24 21:01:53 +0200

    vdsm-tool: Add format-xleases command
    This command can be use to format the xleases special volume if the
    volume is corrupted or there is a need to add the volume on a
    development setup.
    
    The caller must know the path to the volume and make sure the volume is
    accessible (activate the lv if needed).
    
    Example usage:
    
        $ vdsm-tool format-xleases -h
        usage: vdsm-tool [-h] sd_id path
    
        positional arguments:
          sd_id       storage domain UUID
          path        path to xleases volume
    
        optional arguments:
          -h, --help  show this help message and exit
    
    Creating xleases volume on file storage:
    
        PATH=/rhev/data-center/mnt/server:_export/sd_id/dom_md/xleases
        truncate -s 1G $PATH
        vdsm-tool format-xleases sd_id $PATH
    
    Creating the xleases volume on block storage:
    
        lvcreate --name xleases --size 1G sd_id
        vdsm-tool format-xleases sd_id /dev/sd_id/xleases
        lvchange -an sd_id/xleases
    
    Change-Id: I5d1a1a34eea80ddca01967c0a2da2e9e50e50656
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ lib/vdsm/tool/xleases.py
@@ -0,0 +1,61 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+import argparse
+
+from vdsm import utils
+from vdsm.storage import xlease
+from . import expose
+
+
+@expose('format-xleases')
+def format_xleases(*args):
+    """
+    format-xleases sd_id path
+
+    This is a destructive operation, all the leases on this volume will be
+    deleted. This must not be used on an active storage domain.
+
+    The caller is responsible to activate the lv on block storage before
+    calling this and deactivating the lv afterwards.
+
+    If this fails, the volume will not be usable (it will be marked as
+    "updating"), but the operation can be tried again.
+
+    Creating xleases volume on file storage:
+
+        PATH=/rhev/data-center/mnt/server:_export/sd_id/dom_md/xleases
+        truncate -s 1G $PATH
+        vdsm-tool format-xleases sd_id $PATH
+
+    Creating the xleases volume on block storage:
+
+        lvcreate --name xleases --size 1g sd_id
+        vdsm-tool format-xleases sd_id /dev/sd_id/xleases
+        lvchange -an sd_id/xleases
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sd_id', help="storage domain UUID")
+    parser.add_argument('path', help="path to xleases volume")
+    args = parser.parse_args(args[1:])
+
+    backend = xlease.DirectFile(args.path)
+    with utils.closing(backend):
+        xlease.format_index(args.sd_id, backend)
