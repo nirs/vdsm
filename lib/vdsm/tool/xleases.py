caf7bf2a9b9f503fd5c5452d08627320d996c064
Nir Soffer <nsoffer@redhat.com>
2016-12-27 15:47:49 +0200

    vdsm-tool: Add rebuild-xleases command
    This command can be used to synchronize the external leases volume index
    with the actual sanlock leases on storage. This may be needed if the
    index was corrupted.
    
    Example usage:
    
        $ vdsm-tool rebuild-xleases -h
        usage: vdsm-tool [-h] sd_id path
    
        positional arguments:
          sd_id       storage domain UUID
          path        path to xleases volume
    
        optional arguments:
          -h, --help  show this help message and exit
    
    Rebuilding xleases volume on file storage:
    
        PATH=/rhev/data-center/mnt/server:_export/sd_id/dom_md/xleases
        vdsm-tool rebuild-xleases sd_id $PATH
    
    Rebuilding the xleases volume on block storage:
    
        lvchange -ay sd_id/xleases
        vdsm-tool rebuild-xleases sd_id /dev/sd_id/xleases
        lvchange -an sd_id/xleases
    
    Change-Id: I488e0f198b56b30f730de1bb53a6957fe9e3d99f
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- lib/vdsm/tool/xleases.py
+++ lib/vdsm/tool/xleases.py
@@ -51,11 +51,45 @@ def format_xleases(*args):
         vdsm-tool format-xleases sd_id /dev/sd_id/xleases
         lvchange -an sd_id/xleases
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('sd_id', help="storage domain UUID")
-    parser.add_argument('path', help="path to xleases volume")
-    args = parser.parse_args(args[1:])
-
+    args = parse_args(args)
     backend = xlease.DirectFile(args.path)
     with utils.closing(backend):
         xlease.format_index(args.sd_id, backend)
+
+
+@expose('rebuild-xleases')
+def rebuild_xleases(*args):
+    """
+    rebuild-xleases sd_id path
+
+    This operation synchronize the xleases index with the actual leases on
+    storage. This must not be used on an active storage domain.
+
+    The caller is responsible to activate the lv on block storage before
+    calling this and deactivating the lv afterwards.
+
+    If this fails, the volume will not be usable (it will be marked as
+    "updating"), but the operation can be tried again.
+
+    Rebuilding xleases volume on file storage:
+
+        PATH=/rhev/data-center/mnt/server:_export/sd_id/dom_md/xleases
+        vdsm-tool rebuild-xleases sd_id $PATH
+
+    Rebuilding the xleases volume on block storage:
+
+        lvchange -ay sd_id/xleases
+        vdsm-tool rebuild-xleases sd_id /dev/sd_id/xleases
+        lvchange -an sd_id/xleases
+    """
+    args = parse_args(args)
+    backend = xlease.DirectFile(args.path)
+    with utils.closing(backend):
+        xlease.rebuild_index(args.sd_id, backend)
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sd_id', help="storage domain UUID")
+    parser.add_argument('path', help="path to xleases volume")
+    return parser.parse_args(args[1:])
